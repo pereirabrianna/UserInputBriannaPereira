4efeefde2b813784dd80c673e8fbceddb13e87aa
Brianna Pereira <bpereira2@usf.edu>
2020-01-30 19:09:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserInputBriannaPereira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name and Press ENTER ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi, this is " + name);
+            Console.ReadKey();
+
+        }
+    }
+}
